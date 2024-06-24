86b1f1f57b331cdf9d1947a2895df4fcfe8a4052
bartek 'basz' szurgot <git@baszerr.eu>
2024-06-24 19:48:01 +0200

    fixed indentation

--- board/sw/mtest/blink.cpp
+++ board/sw/mtest/blink.cpp
@@ -2,15 +2,15 @@
 
 int main()
 {
-    const uint LED_PIN = 4;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+  const uint LED_PIN = 4;
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    while (true)
-    {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-    }
+  while (true)
+  {
+    gpio_put(LED_PIN, 1);
+    sleep_ms(250);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(250);
+  }
 }
