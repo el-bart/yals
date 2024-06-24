86ba4a449fcccdefb7f7e6c1a6c8c7a3673ed16c
bartek 'basz' szurgot <git@baszerr.eu>
2024-06-24 22:25:39 +0200

    added reading character from uart and removed LED ctrl

--- board/sw/mtest/serial.cpp
+++ board/sw/mtest/serial.cpp
@@ -4,10 +4,6 @@
 
 int main()
 {
-  const auto led_pin = 4;
-  gpio_init(led_pin);
-  gpio_set_dir(led_pin, GPIO_OUT);
-
   auto uart = uart0;
   const auto uart_pin_tx = 0;
   const auto uart_pin_rx = 1;
@@ -19,14 +15,13 @@ int main()
   stdio_init_all();
   printf("--------------------\n"); // standard I/O
 
-  auto led_state = true;
   for(auto n=0u; ; ++n)
   {
     char buf[64];
     snprintf(buf, sizeof(buf), "hello, serial #%u!\n", n);
     uart_puts(uart, buf);   // explicit output
-    sleep_ms(100);
-    gpio_put(led_pin, led_state);
-    led_state ^= true;
+
+    auto const c = uart_getc(uart);
+    printf("got '%c' character\n", c);
   }
 }
