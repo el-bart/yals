9fec0a1f2a28256b8cd7729791ac81f2d010907a
bartek 'basz' szurgot <git@baszerr.eu>
2024-06-24 21:54:34 +0200

    common var for delay (50% duty cycle)

--- board/sw/mtest/blink.cpp
+++ board/sw/mtest/blink.cpp
@@ -8,9 +8,10 @@ int main()
 
   while (true)
   {
+    auto const delay = 250;
     gpio_put(LED_PIN, 1);
-    sleep_ms(250);
+    sleep_ms(delay);
     gpio_put(LED_PIN, 0);
-    sleep_ms(250);
+    sleep_ms(delay);
   }
 }
