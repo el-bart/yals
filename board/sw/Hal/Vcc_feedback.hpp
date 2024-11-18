30145d3646fd7c2a579e4b0262b191b7eb708f44
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-18 14:00:13 +0100

    Vcc feedback mtest is using HAL now

---
+++ board/sw/Hal/Vcc_feedback.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "hardware/adc.h"
+#include "pico/stdlib.h"
+
+namespace Hal
+{
+
+struct Vcc_feedback
+{
+  Vcc_feedback()
+  {
+    adc_init();
+    adc_gpio_init(pin);
+  }
+
+  Vcc_feedback(Vcc_feedback const&) = delete;
+  Vcc_feedback& operator=(Vcc_feedback const&) = delete;
+  Vcc_feedback(Vcc_feedback &&) = delete;
+  Vcc_feedback& operator=(Vcc_feedback &&) = delete;
+
+  float read_volts()
+  {
+    adc_select_input(adc_n);
+    auto const sample = adc_read();
+
+    constexpr float adc_conversion_factor = 3.3f / (1 << 12);       // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+    constexpr float div_conversion_factor = 1.0f + 100.0f / 22.0f;  // resistor divider: Vcc -> 100k -> ADC -> 22k -> GND
+    auto const div_volts = sample * adc_conversion_factor;
+    auto const vcc = div_volts * div_conversion_factor;
+    return vcc;
+  }
+
+private:
+  static constexpr uint adc_n = 1;
+  static constexpr uint pin = 26 + adc_n;
+};
+
+}
--- board/sw/mtest/adc_vcc.cpp
+++ board/sw/mtest/adc_vcc.cpp
@@ -1,36 +1,20 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/uart.h"
-#include "hardware/adc.h"
-
-// ADC 1:
-constexpr auto ADC_N = 1;
-constexpr auto ADC_PIN = 26 + ADC_N;
+#include "Hal/Vcc_feedback.hpp"
+#include "Hal/Uart.hpp"
+#include <cstdio>
 
 int main()
 {
-  adc_init();
-  adc_gpio_init(ADC_PIN);
-  adc_select_input(ADC_N);
+  Hal::Vcc_feedback vccf;
+  Hal::Uart uart;
 
-  auto uart = uart0;
-  constexpr auto uart_pin_tx = 0;
-  constexpr auto uart_pin_rx = 1;
-  constexpr auto uart_baud = 115200;
-  uart_init(uart, uart_baud);
-  gpio_set_function(uart_pin_tx, GPIO_FUNC_UART);
-  gpio_set_function(uart_pin_rx, GPIO_FUNC_UART);
-  stdio_init_all();
-  printf("ADC test for Vcc\n");
+  uart.tx("ADC test for Vcc\r\n");
 
   while(true)
   {
-    auto const v = adc_read();
-    constexpr float adc_conversion_factor = 3.3f / (1 << 12); // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
-    constexpr float div_conversion_factor = 1.0f + 100.0f / 22.0f;   // resistor divider: Vcc -> 100k -> ADC -> 22k -> GND
-    auto const f = v * adc_conversion_factor;
-    auto const vcc = f * div_conversion_factor;
-    printf("u=%u f=%f V=%f\n", v, f, vcc);
+    const auto vcc = vccf.read_volts();
+    char buf[32];
+    snprintf(buf, sizeof(buf), "V=%f\r\n", vcc);
+    uart.tx(buf);
     sleep_ms(500);
   }
 }
