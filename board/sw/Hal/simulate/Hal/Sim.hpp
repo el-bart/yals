3ec48f4e741867f7fc967b62badd4559319262e5
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-18 16:56:20 +0100

    added positon updating to simulation

--- board/sw/Hal/simulate/Hal/Sim.hpp
+++ board/sw/Hal/simulate/Hal/Sim.hpp
@@ -8,12 +8,13 @@ namespace Hal
 struct Sim
 {
   // this updated world simulation state, based on current presets, each time it's set
-  void update()
+  void update(float dt_sec)
   {
     update_amps();
+    update_pos(dt_sec);
   }
 
-  int32_t engine_force_{0}; // -/+ is dir, abs value is a force
+  int32_t engine_force_{0}; // -/+ is dir, abs value is a force (16-bit)
   float amps_{0};           // engien current [A]
   float led_brightness_{0}; // 0..1 of power
   float position_{0};       // 0..1 of scale
@@ -28,6 +29,14 @@ private:
     if( engine_force_ == 0 )
       return;
     amps_ += abs(engine_force_) / 65535.0f * 0.5f
+  }
+
+  void update_pos(float dt_sec)
+  {
+    const auto f = engine_force_ / 65535.0f;
+    const auto full_travell_sec = 2.0f;
+    const auto delta = f / full_travel_time * dt_sec;
+    position_ = std::clamp(position + delta, 0.0f, 1.0f);
   }
 };
 
