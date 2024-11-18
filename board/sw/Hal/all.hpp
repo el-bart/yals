afca828e81e67d15c863aca2e8fa91d11bab62af
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-18 17:08:34 +0100

    added UTs support for native build

---
+++ board/sw/Hal/all.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "Hal/Engine.hpp"
+#include "Hal/Engine_current.hpp"
+#include "Hal/Led.hpp"
+#include "Hal/Position_feedback.hpp"
+#include "Hal/Uart.hpp"
+#include "Hal/Vcc_feedback.hpp"
+
+namespace Hal
+{
+
+struct All
+{
+  Led led_;
+  Uart uart_;
+  Engine eng_;
+  Engine_current eng_current_;
+  Position_feedback pos_;
+  Vcc_feedback vcc_;
+};
+
+}
---
+++ board/sw/sim.ut.cpp
@@ -0,0 +1,11 @@
+#include "catch2/catch.hpp"
+
+namespace
+{
+
+TEST_CASE("test1")
+{
+  REQUIRE(1 == 2);
+}
+
+}
