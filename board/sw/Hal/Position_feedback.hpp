6308080795f8cb8a9115dc08ddd98ad0b8ad6d5f
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-18 13:39:35 +0100

    added mtest for position feedback

---
+++ board/sw/Hal/Position_feedback.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "hardware/adc.h"
+#include "pico/stdlib.h"
+
+namespace Hal
+{
+
+struct Position_feedback
+{
+  Position_feedback()
+  {
+    adc_init();
+    adc_gpio_init(pin);
+  }
+
+  Position_feedback(Position_feedback const&) = delete;
+  Position_feedback& operator=(Position_feedback const&) = delete;
+  Position_feedback(Position_feedback &&) = delete;
+  Position_feedback& operator=(Position_feedback &&) = delete;
+
+  float read_percent()
+  {
+    adc_select_input(adc_n);
+    auto const sample = adc_read();
+    constexpr float adc_conversion_factor = 3.3f / (1 << 12); // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+    auto const voltage = sample * adc_conversion_factor;
+    auto const percent = voltage / 3.3f * 100.0f;
+    return percent;
+  }
+
+private:
+  static constexpr uint adc_n = 0;
+  static constexpr uint pin = 26 + adc_n;
+};
+
+}
