a2e41a66f30b67b0706d65853efe3a0accf8b1a7
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-20 23:37:04 +0100

    nicer split into 2 files, to avoid generating a new one

--- board/sw/cmake/git.cmake
+++ board/sw/cmake/git.cmake
@@ -1,22 +1,9 @@
-file(WRITE "${CMAKE_BINARY_DIR}/version_proxy.cmake"
-"execute_process(COMMAND git describe --always --abbrev=10 --dirty
-  WORKING_DIRECTORY \"${CMAKE_SOURCE_DIR}\"
-  OUTPUT_VARIABLE GIT_VERSION_INFO
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-configure_file(\"\${SRC}\" \"\${DST}\" @ONLY)
-")
-
 include_directories(${CMAKE_BINARY_DIR})
 
 add_custom_target(version_proxy
- "${CMAKE_COMMAND}" -D "SRC=${CMAKE_CURRENT_SOURCE_DIR}/Utils/version.cpp.in"
-                    -D "DST=${CMAKE_CURRENT_BINARY_DIR}/Utils/version.cpp"
-                    -P "${CMAKE_BINARY_DIR}/version_proxy.cmake"
-  SOURCE     "${CMAKE_CURRENT_SOURCE_DIR}/Utils/version.cpp.in"
-  BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/Utils/version.cpp"
+ "${CMAKE_COMMAND}" -D "SRC=${CMAKE_SOURCE_DIR}/Utils/version.cpp.in"
+                    -D "DST=${CMAKE_BINARY_DIR}/Utils/version.cpp"
+                    -P "${CMAKE_SOURCE_DIR}/cmake/version_proxy.cmake"
+  SOURCE     "${CMAKE_SOURCE_DIR}/Utils/version.cpp.in"
+  BYPRODUCTS "${CMAKE_BINARY_DIR}/Utils/version.cpp"
 )
-
-#configure_file("${CMAKE_CURRENT_SOURCE_DIR}/Utils/version.cpp.in"
-#               "${CMAKE_CURRENT_BINARY_DIR}/Utils/version.cpp"
-#                @ONLY)
---
+++ board/sw/cmake/version_proxy.cmake
@@ -0,0 +1,6 @@
+execute_process(COMMAND git describe --always --abbrev=10 --dirty
+  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+  OUTPUT_VARIABLE GIT_VERSION_INFO
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+configure_file("${SRC}" "${DST}" @ONLY)
