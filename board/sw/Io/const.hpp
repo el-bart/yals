b3c0c70220180b46ec42f9861187f5161a4a9fa9
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-20 22:32:38 +0100

    enlarged max line lenght

--- board/sw/Io/const.hpp
+++ board/sw/Io/const.hpp
@@ -2,6 +2,6 @@
 
 namespace Io
 {
-constexpr size_t max_line_size   = 31;
+constexpr size_t max_line_size   = 50; // keep it relatively large, as version string is >30B
 constexpr size_t max_buffer_size = 8 * ( max_line_size + 1 );
 }
