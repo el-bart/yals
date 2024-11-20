78e6fd0170bb69cc5be251b0da0b3e7278044273
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-20 23:39:17 +0100

    added more comments and nicer formatting

--- board/sw/cmake/git.cmake
+++ board/sw/cmake/git.cmake
@@ -1,5 +1,9 @@
+# this is a rework of a concept from:
+# https://cmake.org/pipermail/cmake/2010-July/038015.html
+
 include_directories(${CMAKE_BINARY_DIR})
 
+# defines always-outdated target, that will however only change files when new content is available
 add_custom_target(version_proxy
  "${CMAKE_COMMAND}" -D "SRC=${CMAKE_SOURCE_DIR}/Utils/version.cpp.in"
                     -D "DST=${CMAKE_BINARY_DIR}/Utils/version.cpp"
--- board/sw/cmake/version_proxy.cmake
+++ board/sw/cmake/version_proxy.cmake
@@ -1,6 +1,9 @@
+# regular generator. to be called from git.cmake
+
 execute_process(COMMAND git describe --always --abbrev=10 --dirty
   WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
   OUTPUT_VARIABLE GIT_VERSION_INFO
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+
 configure_file("${SRC}" "${DST}" @ONLY)
