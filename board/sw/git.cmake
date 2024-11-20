842f7a128e389eaf2e105775d391b672f4d35124
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-20 23:19:21 +0100

    changed version.cpp generation scheme

--- board/sw/git.cmake
+++ board/sw/git.cmake
@@ -1,10 +1,21 @@
-execute_process(COMMAND
+file(WRITE "${CMAKE_BINARY_DIR}/version_proxy.cmake"
+"execute_process(COMMAND
   git describe --always --abbrev=10 --dirty
-  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+  WORKING_DIRECTORY \"${CMAKE_SOURCE_DIR}\"
   OUTPUT_VARIABLE GIT_VERSION_INFO
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+CONFIGURE_FILE(\"\${SRC}\" \"\${DST}\" @ONLY)
+")
 
-configure_file("${CMAKE_CURRENT_SOURCE_DIR}/Utils/version.cpp.in"
-               "${CMAKE_CURRENT_BINARY_DIR}/Utils/version.cpp"
-                @ONLY)
+include_directories(${CMAKE_BINARY_DIR})
+
+add_custom_target(version_proxy
+    "${CMAKE_COMMAND}" -D "SRC=${CMAKE_CURRENT_SOURCE_DIR}/Utils/version.cpp.in"
+                       -D "DST=${CMAKE_CURRENT_BINARY_DIR}/Utils/version.cpp"
+                       -P "${CMAKE_BINARY_DIR}/version_proxy.cmake"
+)
+
+#configure_file("${CMAKE_CURRENT_SOURCE_DIR}/Utils/version.cpp.in"
+#               "${CMAKE_CURRENT_BINARY_DIR}/Utils/version.cpp"
+#                @ONLY)
