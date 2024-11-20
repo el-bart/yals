f6674eb202943d77a0276d0f20d371877aa65932
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-20 21:06:47 +0100

    EEPROM sim is now properly handing numerics

--- board/sw/Hal/simulate/Hal/Impl/EEPROM.hpp
+++ board/sw/Hal/simulate/Hal/Impl/EEPROM.hpp
@@ -21,9 +21,9 @@ struct EEPROM
     switch(slot)
     {
       case 0: sim().marker_ = value; return true;
-      case 1: sim().min_position_ = value / u32_max_f; return true;
-      case 2: sim().max_position_ = value / u32_max_f; return true;
-      case 3: sim().EEPROM_LED_brightness_ = value / u32_max_f; return true;
+      case 1: sim().min_position_ = value / u32_max_d; return true;
+      case 2: sim().max_position_ = value / u32_max_d; return true;
+      case 3: sim().EEPROM_LED_brightness_ = value / u32_max_d; return true;
     }
     throw std::runtime_error{"EEPROM::write(): unknown slot: " + std::to_string(slot)};
   }
@@ -33,16 +33,21 @@ struct EEPROM
     switch(slot)
     {
       case 0: return sim().marker_;
-      case 1: return roundf( sim().min_position_ * u32_max_i );
-      case 2: return roundf( sim().max_position_ * u32_max_i );
-      case 3: return roundf( sim().EEPROM_LED_brightness_ * u32_max_i );
+      case 1: return float_to_uint32( sim().min_position_ );
+      case 2: return float_to_uint32( sim().max_position_ );
+      case 3: return float_to_uint32( sim().EEPROM_LED_brightness_ );
     }
     throw std::runtime_error{"EEPROM::read(): unknown slot: " + std::to_string(slot)};
   }
 
 private:
   static constexpr auto u32_max_i = std::numeric_limits<uint32_t>::max();
-  static constexpr auto u32_max_f = static_cast<float>(u32_max_i);
+  static constexpr auto u32_max_d = static_cast<double>(u32_max_i); // need doubles here to avoid numerical fluxes...
+
+  static uint32_t float_to_uint32(float v)
+  {
+    return std::clamp( round( v * u32_max_d ), 0.0, u32_max_d );
+  }
 };
 
 }
