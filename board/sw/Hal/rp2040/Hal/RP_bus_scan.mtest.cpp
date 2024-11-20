6b037c52f2194ed6c391aeb521a598ee174f0cd0
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-20 12:01:51 +0100

    RP2040 I2C bus scanner

---
+++ board/sw/Hal/rp2040/Hal/RP_bus_scan.mtest.cpp
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * inc. changes from BaSz (C++ things and wider address scanning range)
+ */
+
+// Sweep through all 7-bit I2C addresses, to see if any slaves are present on
+// the I2C bus. Print out a table that looks like this:
+//
+// I2C Bus Scan
+//   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+// 0
+// 1       @
+// 2
+// 3             @
+// 4
+// 5
+// 6
+// 7
+//
+// E.g. if slave addresses 0x12 and 0x34 were acknowledged.
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+
+static constexpr auto pin_i2c_sda = 24;
+static constexpr auto pin_i2c_scl = 25;
+static constexpr auto i2c_dev = i2c0;
+static constexpr auto i2c_speed_Hz = 100'000;
+static constexpr auto i2c_byte_timeout_us = 100'000;
+
+// I2C reserves some addresses for special purposes. We exclude these from the scan.
+// These are any addresses of the form 000 0xxx or 111 1xxx
+bool reserved_addr(uint8_t addr) {
+    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+}
+
+int main() {
+    // Enable UART so we can print status output
+    stdio_init_all();
+
+#if 0
+    i2c_init(i2c_dev, 100 * 1000);
+    gpio_set_function(pin_i2c_sda, GPIO_FUNC_I2C);
+    gpio_set_function(pin_i2c_scl, GPIO_FUNC_I2C);
+    gpio_pull_up(pin_i2c_sda);
+    gpio_pull_up(pin_i2c_scl);
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(pin_i2c_sda, pin_i2c_scl, GPIO_FUNC_I2C));
+#else
+    gpio_init(pin_i2c_sda);
+    gpio_init(pin_i2c_scl);
+    gpio_pull_up(pin_i2c_sda);
+    gpio_pull_up(pin_i2c_scl);
+    gpio_set_function(pin_i2c_sda, GPIO_FUNC_I2C);
+    gpio_set_function(pin_i2c_scl, GPIO_FUNC_I2C);
+    i2c_init(i2c_dev, i2c_speed_Hz);
+#endif
+
+    printf("\nI2C Bus Scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+    for (int addr = 0; addr < 256; ++addr) {
+        if (addr % 16 == 0) {
+            printf("%02x ", addr);
+        }
+
+        // Perform a 1-byte dummy read from the probe address. If a slave
+        // acknowledges this address, the function returns the number of bytes
+        // transferred. If the address byte is ignored, the function returns
+        // -1.
+
+        // Skip over any reserved addresses.
+        int ret;
+        uint8_t rxdata;
+        if (reserved_addr(addr))
+            ret = PICO_ERROR_GENERIC;
+        else
+            ret = i2c_read_blocking(i2c_dev, addr, &rxdata, 1, false);
+
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : "  ");
+    }
+    printf("Done.\n");
+    return 0;
+}
