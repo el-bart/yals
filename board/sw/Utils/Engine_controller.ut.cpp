8686fd7fd87bc21414a2a560a75bbb796a5749f0
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-21 17:31:38 +0100

    algo for progressing with engine movement is now a separate code

--- board/sw/Utils/Engine_controller.ut.cpp
+++ board/sw/Utils/Engine_controller.ut.cpp
@@ -10,9 +10,39 @@ using namespace Utils::Config;
 namespace
 {
 
+bool sim_move_to(EC& ec, float const setpoint)
+{
+  auto prev_delta_pos  = setpoint - sim().position_;
+  auto prev_pos_offset = setpoint - sim().position_;
+  for(auto t = 0.0; t < Hal::Sim::eng_full_travel_time_s; t += control_loop_time)
+  {
+    auto const prev_pos = sim().position_;
+    ec.update(setpoint, sim().position_);
+    sim().update(control_loop_time);
+
+    auto const delta_pos  = sim().position_ - prev_pos;
+    auto const pos_offset = setpoint - sim().position_;
+    INFO("t="       << t)
+      INFO("pos="     << sim().position_);
+    INFO("force="   << sim().engine_force_ / 65535.0);
+    INFO("d_pos="   << delta_pos);
+    INFO("pos_off=" << pos_offset);
+    CHECK( fabs(delta_pos) <= fabs(prev_delta_pos) );
+    CHECK( fabs(pos_offset) <= fabs(prev_pos_offset) );
+    prev_delta_pos  = delta_pos;
+    prev_pos_offset = pos_offset;
+    // uncomment to get a trace of the numbers on each iteration:
+    //CHECK(false);
+
+    if( sim().engine_force_ == 0 )
+      return true;
+  }
+  return false;
+}
+
+
 TEST_CASE("Engine_controller")
 {
-  auto constexpr eng_full_travel_time_s = Hal::Sim::eng_full_travel_time_s;
   sim().reset();
   sim().position_ = 0.5;
   Hal::Engine eng;
@@ -52,34 +82,14 @@ TEST_CASE("Engine_controller")
     auto const setpoint = 0.75f;
     INFO("setpoint=" << setpoint);
     INFO("servo_position_tolerance=" << servo_position_tolerance);
-    auto prev_delta_pos  = setpoint - sim().position_;
-    auto prev_pos_offset = setpoint - sim().position_;
-    for(auto t = 0.0; t < eng_full_travel_time_s; t += control_loop_time)
-    {
-      auto const prev_pos = sim().position_;
-      ec.update(setpoint, sim().position_);
-      sim().update(control_loop_time);
-
-      auto const delta_pos  = sim().position_ - prev_pos;
-      auto const pos_offset = setpoint - sim().position_;
-      INFO("t="       << t)
-      INFO("pos="     << sim().position_);
-      INFO("force="   << sim().engine_force_ / 65535.0);
-      INFO("d_pos="   << delta_pos);
-      INFO("pos_off=" << pos_offset);
-      CHECK( fabs(delta_pos) <= fabs(prev_delta_pos) );
-      CHECK( fabs(pos_offset) <= fabs(prev_pos_offset) );
-      prev_delta_pos  = delta_pos;
-      prev_pos_offset = pos_offset;
-      // uncomment to get a trace of the numbers on each iteration:
-      //CHECK(false);
-    }
+    sim_move_to(ec, setpoint);
     CHECK( sim().position_ == Approx(setpoint).margin(servo_position_tolerance) );
     CHECK( sim().engine_force_ == 0u );
   }
 
   SECTION("histeresis support")
   {
+
     SECTION("reaching value and not moving until position is outside of histeresis")
     {
       // TODO
