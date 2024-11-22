c3fd394cc9c145948beae27e73dc28474a45c85c
bartek 'basz' szurgot <git@baszerr.eu>
2024-11-22 14:42:23 +0100

    added watchdog mtest with a limitation for reboots via weatchdog

---
+++ board/sw/Hal/rp2040/Hal/Watchdog.mtest.cpp
@@ -0,0 +1,33 @@
+#include "Hal/Watchdog.hpp"
+#include "Hal/Uart.hpp"
+#include "Utils/Config/settings.hpp"
+#include "Utils/write_helpers.hpp"
+
+using Utils::write_line;
+using Utils::write_line_fmt;
+
+int main()
+{
+  Hal::Uart uart;
+
+  write_line(uart, ">>");
+  write_line(uart, ">> watchdog app");
+  write_line(uart, ">>");
+  Utils::purge_rx(uart);
+
+  if( watchdog_caused_reboot() )
+  {
+    write_line(uart, ">> REBOOT WAS CAUSED BY WATCHDOG!");
+    write_line(uart, ">> STOPPING APP NOW TO PREVENT BUSY LOOP (YOU ARE WELCOME");
+    while(true) { }
+  }
+
+  Hal::Watchdog watchdog;
+
+  for(auto n=0u; true; ++n)
+  {
+    write_line_fmt(uart, ">> cycle length %u[ms]", n);
+    sleep_ms(n);
+    watchdog.reset();
+  }
+}
