d6601737575583a2852e30f7c1431b9c7f0e750a
bartek 'basz' szurgot <git@baszerr.eu>
2024-12-11 08:53:40 +0100

    added static_assert for safe watchdog value, to ensure some sanity checks are in place

--- board/sw/Hal/rp2040/Hal/Impl/Watchdog.hpp
+++ board/sw/Hal/rp2040/Hal/Impl/Watchdog.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <hardware/watchdog.h>
 #include "Utils/Config/settings.hpp"
+#include "Hal/Impl/EEPROM.hpp"
 
 namespace Hal::Impl
 {
@@ -14,7 +15,10 @@ struct Watchdog
     auto constexpr MHzs = XOSC_KHZ / 1'000u;
     watchdog_start_tick(MHzs);
 
-    auto constexpr delay_ms = watchdog_allow_cycles_skip * control_loop_time_s * 1'000u;
+    auto constexpr control_loop_time_ms = control_loop_time_s * 1'000u;
+    auto constexpr value_write_time_ms = sizeof(EEPROM::value_type) * EEPROM::delay_to_write_data_ms;
+    auto constexpr delay_ms = watchdog_allow_cycles_skip * control_loop_time_ms;
+    static_assert( delay_ms > value_write_time_ms + 2*control_loop_time_ms );
     auto constexpr pause_on_debug = false;
     watchdog_enable(delay_ms, pause_on_debug);
   }
